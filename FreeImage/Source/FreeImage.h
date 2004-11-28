4bd438b4d6b55404ef13502fac9872a092dbba31
drolon <drolon@users.noreply.github.com>
2004-11-28 15:21:55 +0000

    Release 3.5.2

--- FreeImage/Source/FreeImage.h
+++ FreeImage/Source/FreeImage.h
@@ -48,7 +48,7 @@
 
 #define FREEIMAGE_MAJOR_VERSION   3
 #define FREEIMAGE_MINOR_VERSION   5
-#define FREEIMAGE_RELEASE_SERIAL  1
+#define FREEIMAGE_RELEASE_SERIAL  2
 
 // Compiler options ---------------------------------------------------------
 
