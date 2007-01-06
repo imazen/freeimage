6a81a25742e711f0fc47bff58fe998908797a413
drolon <drolon@users.noreply.github.com>
2007-01-06 20:35:21 +0000

    changed #include <stdint.h> by #include <inttypes.h> in FreeImage.h (needed by Solaris 9)

--- FreeImage/Source/FreeImage.h
+++ FreeImage/Source/FreeImage.h
@@ -126,7 +126,7 @@ FI_STRUCT (FIMULTIBITMAP) { void *data; };
 
 #ifndef _MSC_VER
 // define portable types for 32-bit / 64-bit OS
-#include <stdint.h>
+#include <inttypes.h>
 typedef int32_t BOOL;
 typedef uint8_t BYTE;
 typedef uint16_t WORD;
