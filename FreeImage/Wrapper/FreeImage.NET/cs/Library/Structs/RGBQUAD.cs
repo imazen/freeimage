e02efd573735239657cee7298413f5637d1fb481
cklein05 <cklein05@users.noreply.github.com>
2008-06-16 15:16:53 +0000

    Added implicit operators converting from and to UInt32. Minor code and documentation updates.

--- FreeImage/Wrapper/FreeImage.NET/cs/Library/Structs/RGBQUAD.cs
+++ FreeImage/Wrapper/FreeImage.NET/cs/Library/Structs/RGBQUAD.cs
@@ -39,74 +39,181 @@ using System.Runtime.InteropServices;
 
 namespace FreeImageAPI
 {
-	// Bitmaps are made up of different structures.
-	// Some bitmaps have palettes that define the used colors and the bitmaps real
-	// data links to the palette index.
-	// Others don't have a palette and each pixel is stored directly.
-	//
-	// No matter which type of bitmap is accessed FreeImage provides pointers to
-	// beginning of a structure and its length.
-	// In unmanaged code pointers would be used to access the data. In .NET
-	// unsafe code is needed to that.
-	//
-	// RGBQUAD, RGBTRIPLE, FIRGB16, FIRGBA16, FIRGBF and FIRGBAF represent
-	// the structures that bitmaps use to store their data.
-	// Its up to the coder to choose the right one.
-	//
-	// Each structure can be converted from and to the .NETs structure 'Color'.
-
 	/// <summary>
-	/// The RGBQUAD structure describes a color consisting of relative intensities of red, green, and blue
-	/// combined with an alpha factor. Each color is using 1 byte of data.
+	/// The <b>RGBQUAD</b> structure describes a color consisting of relative
+	/// intensities of red, green, blue and alpha value. Each single color
+	/// component consumes 8 bits and so, takes values in the range from 0 to 255.
 	/// </summary>
-	[Serializable, StructLayout(LayoutKind.Sequential)]
+	/// <remarks>
+	/// <para>
+	/// The <b>RGBQUAD</b> structure provides access to an underlying Win32 <b>RGBQUAD</b>
+	/// structure. To determine the alpha, red, green or blue component of a color,
+	/// use the rgbReserved, rgbRed, rgbGreen or rgbBlue fields, respectively.
+	/// </para>
+	/// <para>For easy integration of the underlying structure into the .NET framework,
+	/// the <b>RGBQUAD</b> structure implements implicit conversion operators to 
+	/// convert the represented color to and from the <see cref="System.Drawing.Color"/>
+	/// type. This makes the <see cref="System.Drawing.Color"/> type a real replacement
+	/// for the <b>RGBQUAD</b> structure and my be used in all situations which require
+	/// an <b>RGBQUAD</b> type.
+	/// </para>
+	/// <para>
+	/// Each color component rgbReserved, rgbRed, rgbGreen or rgbBlue of <b>RGBQUAD</b>
+	/// is translated into it's corresponding color component A, R, G or B of
+	/// <see cref="System.Drawing.Color"/> by an one-to-one manner and vice versa.
+	/// </para>
+	/// <para>
+	/// <b>Conversion from System.Drawing.Color to RGBQUAD</b>
+	/// </para>
+	/// <c>RGBQUAD.component = Color.component</c>
+	/// <para>
+	/// <b>Conversion from RGBQUAD to System.Drawing.Color</b>
+	/// </para>
+	/// <c>Color.component = RGBQUAD.component</c>
+	/// <para>
+	/// The same conversion is also applied when the <see cref="FreeImageAPI.RGBQUAD.Color"/>
+	/// property or the <see cref="FreeImageAPI.RGBQUAD(System.Drawing.Color)"/> constructor
+	/// is invoked.
+	/// </para>
+	/// </remarks>
+	/// <example>
+	/// The following code example demonstrates the various conversions between the
+	/// <b>RGBQUAD</b> structure and the <see cref="System.Drawing.Color"/> structure.
+	/// <code>
+	/// RGBQUAD rgbq;
+	/// // Initialize the structure using a native .NET Color structure.
+	///	rgbq = new RGBQUAD(Color.Indigo);
+	/// // Initialize the structure using the implicit operator.
+	///	rgbq = Color.DarkSeaGreen;
+	/// // Convert the RGBQUAD instance into a native .NET Color
+	/// // using its implicit operator.
+	///	Color color = rgbq;
+	/// // Using the structure's Color property for converting it
+	/// // into a native .NET Color.
+	///	Color another = rgbq.Color;
+	/// </code>
+	/// </example>
+	[Serializable, StructLayout(LayoutKind.Explicit)]
 	public struct RGBQUAD : IComparable, IComparable<RGBQUAD>, IEquatable<RGBQUAD>
 	{
+		/// <summary>
+		/// The blue color component.
+		/// </summary>
+		[FieldOffset(0)]
 		public byte rgbBlue;
+
+		/// <summary>
+		/// The green color component.
+		/// </summary>
+		[FieldOffset(1)]
 		public byte rgbGreen;
+
+		/// <summary>
+		/// The red color component.
+		/// </summary>
+		[FieldOffset(2)]
 		public byte rgbRed;
+
+		/// <summary>
+		/// The alpha color component.
+		/// </summary>
+		[FieldOffset(3)]
 		public byte rgbReserved;
 
 		/// <summary>
-		/// Create a new instance.
+		/// The color's value.
 		/// </summary>
-		/// <param name="color">Color to initialize with.</param>
+		[FieldOffset(0)]
+		public uint uintValue;
+
+		/// <summary>
+		/// Initializes a new instance based on the specified <see cref="System.Drawing.Color"/>.
+		/// </summary>
+		/// <param name="color"><see cref="System.Drawing.Color"/> to initialize with.</param>
 		public RGBQUAD(Color color)
 		{
+			uintValue = 0u;
 			rgbBlue = color.B;
 			rgbGreen = color.G;
 			rgbRed = color.R;
 			rgbReserved = color.A;
 		}
 
-		public static bool operator ==(RGBQUAD value1, RGBQUAD value2)
+		/// <summary>
+		/// Tests whether two specified <see cref="RGBQUAD"/> structures are equivalent.
+		/// </summary>
+		/// <param name="left">The <see cref="RGBQUAD"/> that is to the left of the equality operator.</param>
+		/// <param name="right">The <see cref="RGBQUAD"/> that is to the right of the equality operator.</param>
+		/// <returns>
+		/// <b>true</b> if the two <see cref="RGBQUAD"/> structures are equal; otherwise, <b>false</b>.
+		/// </returns>
+		public static bool operator ==(RGBQUAD left, RGBQUAD right)
 		{
-			return
-				value1.rgbBlue == value2.rgbBlue &&
-				value1.rgbGreen == value2.rgbGreen &&
-				value1.rgbRed == value2.rgbRed &&
-				value1.rgbReserved == value2.rgbReserved;
-		}
-
-		public static bool operator !=(RGBQUAD value1, RGBQUAD value2)
-		{
-			return !(value1 == value2);
-		}
-
-		public static implicit operator RGBQUAD(Color color)
-		{
-			return new RGBQUAD(color);
-		}
-
-		public static implicit operator Color(RGBQUAD rgbquad)
-		{
-			return rgbquad.color;
+			return ((left.rgbBlue == right.rgbBlue) &&
+					(left.rgbGreen == right.rgbGreen) &&
+					(left.rgbRed == right.rgbRed) &&
+					(left.rgbReserved == right.rgbReserved));
 		}
 
 		/// <summary>
-		/// Gets or sets the color of the structure.
+		/// Tests whether two specified <see cref="RGBQUAD"/> structures are different.
 		/// </summary>
-		public Color color
+		/// <param name="left">The <see cref="RGBQUAD"/> that is to the left of the inequality operator.</param>
+		/// <param name="right">The <see cref="RGBQUAD"/> that is to the right of the inequality operator.</param>
+		/// <returns>
+		/// <b>true</b> if the two <see cref="RGBQUAD"/> structures are different; otherwise, <b>false</b>.
+		/// </returns>
+		public static bool operator !=(RGBQUAD left, RGBQUAD right)
+		{
+			return !(left == right);
+		}
+
+		/// <summary>
+		/// Converts the value of a <see cref="System.Drawing.Color"/> structure to a <see cref="RGBQUAD"/> structure.
+		/// </summary>
+		/// <param name="value">A <see cref="System.Drawing.Color"/> structure.</param>
+		/// <returns>A new instance of <see cref="RGBQUAD"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator RGBQUAD(Color value)
+		{
+			return new RGBQUAD(value);
+		}
+
+		/// <summary>
+		/// Converts the value of a <see cref="RGBQUAD"/> structure to a Color structure.
+		/// </summary>
+		/// <param name="value">A <see cref="RGBQUAD"/> structure.</param>
+		/// <returns>A new instance of <see cref="System.Drawing.Color"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator Color(RGBQUAD value)
+		{
+			return value.Color;
+		}
+
+		/// <summary>
+		/// Converts the value of an <see cref="UInt32"/> structure to a <see cref="RGBQUAD"/> structure.
+		/// </summary>
+		/// <param name="value">An <see cref="UInt32"/> structure.</param>
+		/// <returns>A new instance of <see cref="RGBQUAD"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator RGBQUAD(uint value)
+		{
+			RGBQUAD result = new RGBQUAD();
+			result.uintValue = value;
+			return result;
+		}
+
+		/// <summary>
+		/// Converts the value of a <see cref="RGBQUAD"/> structure to an <see cref="UInt32"/> structure.
+		/// </summary>
+		/// <param name="value">A <see cref="RGBQUAD"/> structure.</param>
+		/// <returns>A new instance of <see cref="RGBQUAD"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator uint(RGBQUAD value)
+		{
+			return value.uintValue;
+		}
+
+		/// <summary>
+		/// Gets or sets the <see cref="System.Drawing.Color"/> of the structure.
+		/// </summary>
+		public Color Color
 		{
 			get
 			{
@@ -126,46 +233,75 @@ namespace FreeImageAPI
 		}
 
 		/// <summary>
-		/// Compares the current instance with another object of the same type.
+		/// Compares this instance with a specified <see cref="Object"/>.
 		/// </summary>
 		/// <param name="obj">An object to compare with this instance.</param>
-		/// <returns>A 32-bit signed integer that indicates the relative order of the objects being compared.</returns>
+		/// <returns>A 32-bit signed integer indicating the lexical relationship between the two comparands.</returns>
+		/// <exception cref="ArgumentException"><paramref name="obj"/> is not a <see cref="RGBQUAD"/>.</exception>
 		public int CompareTo(object obj)
 		{
-			if (obj is RGBQUAD)
+			if (obj == null)
 			{
-				return CompareTo((RGBQUAD)obj);
+				return 1;
 			}
-			throw new ArgumentException();
+			if (!(obj is RGBQUAD))
+			{
+				throw new ArgumentException();
+			}
+			return CompareTo((RGBQUAD)obj);
 		}
 
 		/// <summary>
-		/// Compares the current instance with another object of the same type.
+		/// Compares this instance with a specified <see cref="RGBQUAD"/> object.
 		/// </summary>
-		/// <param name="other">An object to compare with this instance.</param>
-		/// <returns>A 32-bit signed integer that indicates the relative order of the objects being compared.</returns>
+		/// <param name="other">A <see cref="RGBQUAD"/> to compare.</param>
+		/// <returns>A signed number indicating the relative values of this instance
+		/// and <paramref name="other"/>.</returns>
 		public int CompareTo(RGBQUAD other)
 		{
-			return this.color.ToArgb().CompareTo(other.color.ToArgb());
+			return this.Color.ToArgb().CompareTo(other.Color.ToArgb());
 		}
 
 		/// <summary>
-		/// Indicates whether the current object is equal to another object of the same type.
+		/// Tests whether the specified object is a <see cref="RGBQUAD"/> structure
+		/// and is equivalent to this <see cref="RGBQUAD"/> structure.
 		/// </summary>
-		/// <param name="other">An object to compare with this object.</param>
-		/// <returns>True if the current object is equal to the other parameter; otherwise, false.</returns>
+		/// <param name="obj">The object to test.</param>
+		/// <returns><b>true</b> if <paramref name="obj"/> is a <see cref="RGBQUAD"/> structure
+		/// equivalent to this <see cref="RGBQUAD"/> structure; otherwise, <b>false</b>.</returns>
+		public override bool Equals(object obj)
+		{
+			return ((obj is RGBQUAD) && (this == ((RGBQUAD)obj)));
+		}
+
+		/// <summary>
+		/// Tests whether the specified <see cref="RGBQUAD"/> structure is equivalent to this <see cref="RGBQUAD"/> structure.
+		/// </summary>
+		/// <param name="other">A <see cref="RGBQUAD"/> structure to compare to this instance.</param>
+		/// <returns><b>true</b> if <paramref name="obj"/> is a <see cref="RGBQUAD"/> structure
+		/// equivalent to this <see cref="RGBQUAD"/> structure; otherwise, <b>false</b>.</returns>
 		public bool Equals(RGBQUAD other)
 		{
-			return this == other;
+			return (this == other);
 		}
 
 		/// <summary>
-		/// Serves as a hash function for a particular type.
+		/// Returns a hash code for this <see cref="RGBQUAD"/> structure.
 		/// </summary>
-		/// <returns>A hash code for the current object.</returns>
+		/// <returns>An integer value that specifies the hash code for this <see cref="RGBQUAD"/>.</returns>
 		public override int GetHashCode()
 		{
-			return ((rgbBlue << 16) | (rgbGreen << 8) | (rgbRed));
+			return base.GetHashCode();
+		}
+
+		/// <summary>
+		/// Converts the numeric value of the <see cref="RGBQUAD"/> object
+		/// to its equivalent string representation.
+		/// </summary>
+		/// <returns>The string representation of the value of this instance.</returns>
+		public override string ToString()
+		{
+			return FreeImage.ColorToString(Color);
 		}
 	}
 }
--- FreeImage/Wrapper/FreeImage.NET/cs/Library/Structs/RGBTRIPLE.cs
+++ FreeImage/Wrapper/FreeImage.NET/cs/Library/Structs/RGBTRIPLE.cs
@@ -40,20 +40,85 @@ using System.Runtime.InteropServices;
 namespace FreeImageAPI
 {
 	/// <summary>
-	/// The RGBTRIPLE structure describes a color consisting of relative intensities of red, green, and blue.
-	/// Each color is using 1 byte of data.
+	/// The <b>RGBTRIPLE</b> structure describes a color consisting of relative
+	/// intensities of red, green and blue value. Each single color component
+	/// consumes 8 bits and so, takes values in the range from 0 to 255.
 	/// </summary>
+	/// <remarks>
+	/// <para>
+	/// The <b>RGBTRIPLE</b> structure provides access to an underlying Win32 <b>RGBTRIPLE</b>
+	/// structure. To determine the red, green or blue component of a color, use the
+	/// rgbtRed, rgbtGreen or rgbtBlue fields, respectively.
+	/// </para>
+	/// <para>For easy integration of the underlying structure into the .NET framework,
+	/// the <b>RGBTRIPLE</b> structure implements implicit conversion operators to 
+	/// convert the represented color to and from the <see cref="System.Drawing.Color"/>
+	/// type. This makes the <see cref="System.Drawing.Color"/> type a real replacement
+	/// for the <b>RGBTRIPLE</b> structure and my be used in all situations which require
+	/// an <b>RGBTRIPLE</b> type.
+	/// </para>
+	/// <para>
+	/// Each of the color components rgbtRed, rgbtGreen or rgbtBlue of <b>RGBTRIPLE</b> is
+	/// translated into it's corresponding color component R, G or B of
+	/// <see cref="System.Drawing.Color"/> by an one-to-one manner and vice versa.
+	/// When converting from <see cref="System.Drawing.Color"/> into <b>RGBTRIPLE</b>, the
+	/// color's alpha value is ignored and assumed to be 255 when converting from
+	/// <b>RGBTRIPLE</b> into <see cref="System.Drawing.Color"/>, creating a fully
+	/// opaque color.
+	/// </para>
+	/// <para>
+	/// <b>Conversion from System.Drawing.Color to RGBTRIPLE</b>
+	/// </para>
+	/// <c>RGBTRIPLE.component = Color.component</c>
+	/// <para>
+	/// <b>Conversion from RGBTRIPLE to System.Drawing.Color</b>
+	/// </para>
+	/// <c>Color.component = RGBTRIPLE.component</c>
+	/// <para>
+	/// The same conversion is also applied when the <see cref="FreeImageAPI.RGBTRIPLE.Color"/>
+	/// property or the <see cref="FreeImageAPI.RGBTRIPLE(System.Drawing.Color)"/> constructor
+	/// is invoked.
+	/// </para>
+	/// </remarks>
+	/// <example>
+	/// The following code example demonstrates the various conversions between the
+	/// <b>RGBTRIPLE</b> structure and the <see cref="System.Drawing.Color"/> structure.
+	/// <code>
+	/// RGBTRIPLE rgbt;
+	/// // Initialize the structure using a native .NET Color structure.
+	///	rgbt = new RGBTRIPLE(Color.Indigo);
+	/// // Initialize the structure using the implicit operator.
+	///	rgbt = Color.DarkSeaGreen;
+	/// // Convert the RGBTRIPLE instance into a native .NET Color
+	/// // using its implicit operator.
+	///	Color color = rgbt;
+	/// // Using the structure's Color property for converting it
+	/// // into a native .NET Color.
+	///	Color another = rgbt.Color;
+	/// </code>
+	/// </example>
 	[Serializable, StructLayout(LayoutKind.Sequential)]
 	public struct RGBTRIPLE : IComparable, IComparable<RGBTRIPLE>, IEquatable<RGBTRIPLE>
 	{
+		/// <summary>
+		/// The blue color component.
+		/// </summary>
 		public byte rgbtBlue;
+
+		/// <summary>
+		/// The green color component.
+		/// </summary>
 		public byte rgbtGreen;
+
+		/// <summary>
+		/// The red color component.
+		/// </summary>
 		public byte rgbtRed;
 
 		/// <summary>
-		/// Create a new instance.
+		/// Initializes a new instance based on the specified <see cref="System.Drawing.Color"/>.
 		/// </summary>
-		/// <param name="color">Color to initialize with.</param>
+		/// <param name="color"><see cref="System.Drawing.Color"/> to initialize with.</param>
 		public RGBTRIPLE(Color color)
 		{
 			rgbtBlue = color.B;
@@ -61,33 +126,83 @@ namespace FreeImageAPI
 			rgbtRed = color.R;
 		}
 
-		public static bool operator ==(RGBTRIPLE value1, RGBTRIPLE value2)
+		/// <summary>
+		/// Tests whether two specified <see cref="RGBTRIPLE"/> structures are equivalent.
+		/// </summary>
+		/// <param name="left">The <see cref="RGBTRIPLE"/> that is to the left of the equality operator.</param>
+		/// <param name="right">The <see cref="RGBTRIPLE"/> that is to the right of the equality operator.</param>
+		/// <returns>
+		/// <b>true</b> if the two <see cref="RGBTRIPLE"/> structures are equal; otherwise, <b>false</b>.
+		/// </returns>
+		public static bool operator ==(RGBTRIPLE left, RGBTRIPLE right)
 		{
 			return
-				value1.rgbtBlue == value2.rgbtBlue &&
-				value1.rgbtGreen == value2.rgbtGreen &&
-				value1.rgbtRed == value2.rgbtRed;
-		}
-
-		public static bool operator !=(RGBTRIPLE value1, RGBTRIPLE value2)
-		{
-			return !(value1 == value2);
-		}
-
-		public static implicit operator RGBTRIPLE(Color color)
-		{
-			return new RGBTRIPLE(color);
-		}
-
-		public static implicit operator Color(RGBTRIPLE rgbtripple)
-		{
-			return rgbtripple.color;
+				left.rgbtBlue == right.rgbtBlue &&
+				left.rgbtGreen == right.rgbtGreen &&
+				left.rgbtRed == right.rgbtRed;
 		}
 
 		/// <summary>
-		/// Gets or sets the color of the structure.
+		/// Tests whether two specified <see cref="RGBTRIPLE"/> structures are different.
 		/// </summary>
-		public Color color
+		/// <param name="left">The <see cref="RGBTRIPLE"/> that is to the left of the inequality operator.</param>
+		/// <param name="right">The <see cref="RGBTRIPLE"/> that is to the right of the inequality operator.</param>
+		/// <returns>
+		/// <b>true</b> if the two <see cref="RGBTRIPLE"/> structures are different; otherwise, <b>false</b>.
+		/// </returns>
+		public static bool operator !=(RGBTRIPLE left, RGBTRIPLE right)
+		{
+			return !(left == right);
+		}
+
+		/// <summary>
+		/// Converts the value of a <see cref="System.Drawing.Color"/> structure to a <see cref="RGBTRIPLE"/> structure.
+		/// </summary>
+		/// <param name="value">A <see cref="System.Drawing.Color"/> structure.</param>
+		/// <returns>A new instance of <see cref="RGBTRIPLE"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator RGBTRIPLE(Color value)
+		{
+			return new RGBTRIPLE(value);
+		}
+
+		/// <summary>
+		/// Converts the value of a <see cref="RGBTRIPLE"/> structure to a <see cref="System.Drawing.Color"/> structure.
+		/// </summary>
+		/// <param name="value">A <see cref="RGBTRIPLE"/> structure.</param>
+		/// <returns>A new instance of <see cref="System.Drawing.Color"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator Color(RGBTRIPLE value)
+		{
+			return value.Color;
+		}
+
+		/// <summary>
+		/// Converts the value of an <see cref="UInt32"/> structure to a <see cref="RGBTRIPLE"/> structure.
+		/// </summary>
+		/// <param name="value">An <see cref="UInt32"/> structure.</param>
+		/// <returns>A new instance of <see cref="RGBTRIPLE"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator RGBTRIPLE(uint value)
+		{
+			RGBTRIPLE result = new RGBTRIPLE();
+			result.rgbtBlue = (byte)(value & 0xFF);
+			result.rgbtGreen = (byte)((value >> 8) & 0xFF);
+			result.rgbtRed = (byte)((value >> 16) & 0xFF);
+			return result;
+		}
+
+		/// <summary>
+		/// Converts the value of a <see cref="RGBTRIPLE"/> structure to an <see cref="UInt32"/> structure.
+		/// </summary>
+		/// <param name="value">A <see cref="RGBTRIPLE"/> structure.</param>
+		/// <returns>A new instance of <see cref="RGBTRIPLE"/> initialized to <paramref name="value"/>.</returns>
+		public static implicit operator uint(RGBTRIPLE value)
+		{
+			return (uint)((value.rgbtRed << 16) | (value.rgbtGreen << 8) | (value.rgbtBlue));
+		}
+
+		/// <summary>
+		/// Gets or sets the <see cref="System.Drawing.Color"/> of the structure.
+		/// </summary>
+		public Color Color
 		{
 			get
 			{
@@ -105,46 +220,76 @@ namespace FreeImageAPI
 		}
 
 		/// <summary>
-		/// Compares the current instance with another object of the same type.
+		/// Compares this instance with a specified <see cref="Object"/>.
 		/// </summary>
 		/// <param name="obj">An object to compare with this instance.</param>
-		/// <returns>A 32-bit signed integer that indicates the relative order of the objects being compared.</returns>
+		/// <returns>A 32-bit signed integer indicating the lexical relationship between the two comparands.</returns>
+		/// <exception cref="ArgumentException"><paramref name="obj"/> is not a <see cref="RGBTRIPLE"/>.</exception>
 		public int CompareTo(object obj)
 		{
-			if (obj is RGBTRIPLE)
+			if (obj == null)
 			{
-				return CompareTo((RGBTRIPLE)obj);
+				return 1;
 			}
-			throw new ArgumentException();
+			if (!(obj is RGBTRIPLE))
+			{
+				throw new ArgumentException();
+			}
+			return CompareTo((RGBTRIPLE)obj);
 		}
 
 		/// <summary>
-		/// Compares the current instance with another object of the same type.
+		/// Compares this instance with a specified <see cref="RGBTRIPLE"/> object.
 		/// </summary>
-		/// <param name="other">An object to compare with this instance.</param>
-		/// <returns>A 32-bit signed integer that indicates the relative order of the objects being compared.</returns>
+		/// <param name="other">A <see cref="RGBTRIPLE"/> to compare.</param>
+		/// <returns>A signed number indicating the relative values of this instance
+		/// and <paramref name="other"/>.</returns>
 		public int CompareTo(RGBTRIPLE other)
 		{
-			return this.color.ToArgb().CompareTo(other.color.ToArgb());
+			return this.Color.ToArgb().CompareTo(other.Color.ToArgb());
 		}
 
 		/// <summary>
-		/// Indicates whether the current object is equal to another object of the same type.
+		/// Tests whether the specified object is a <see cref="RGBTRIPLE"/> structure
+		/// and is equivalent to this <see cref="RGBTRIPLE"/> structure.
 		/// </summary>
-		/// <param name="other">An object to compare with this object.</param>
-		/// <returns>True if the current object is equal to the other parameter; otherwise, false.</returns>
+		/// <param name="obj">The object to test.</param>
+		/// <returns><b>true</b> if <paramref name="obj"/> is a <see cref="RGBTRIPLE"/> structure
+		/// equivalent to this <see cref="RGBTRIPLE"/> structure; otherwise, <b>false</b>.</returns>
+		public override bool Equals(object obj)
+		{
+			return ((obj is RGBTRIPLE) && (this == ((RGBTRIPLE)obj)));
+		}
+
+		/// <summary>
+		/// Tests whether the specified <see cref="RGBTRIPLE"/> structure is equivalent to this
+		/// <see cref="RGBTRIPLE"/> structure.
+		/// </summary>
+		/// <param name="other">A <see cref="RGBTRIPLE"/> structure to compare to this instance.</param>
+		/// <returns><b>true</b> if <paramref name="obj"/> is a <see cref="RGBTRIPLE"/> structure
+		/// equivalent to this <see cref="RGBTRIPLE"/> structure; otherwise, <b>false</b>.</returns>
 		public bool Equals(RGBTRIPLE other)
 		{
-			return this == other;
+			return (this == other);
 		}
 
 		/// <summary>
-		/// Serves as a hash function for a particular type.
+		/// Returns a hash code for this <see cref="RGBTRIPLE"/> structure.
 		/// </summary>
-		/// <returns>A hash code for the current object.</returns>
+		/// <returns>An integer value that specifies the hash code for this <see cref="RGBTRIPLE"/>.</returns>
 		public override int GetHashCode()
 		{
-			return ((rgbtBlue << 16) | (rgbtGreen << 8) | (rgbtRed));
+			return base.GetHashCode();
+		}
+
+		/// <summary>
+		/// Converts the numeric value of the <see cref="RGBTRIPLE"/> object
+		/// to its equivalent string representation.
+		/// </summary>
+		/// <returns>The string representation of the value of this instance.</returns>
+		public override string ToString()
+		{
+			return FreeImage.ColorToString(Color);
 		}
 	}
 }
