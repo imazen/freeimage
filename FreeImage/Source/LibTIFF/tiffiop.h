aa80b868af6d3219cdcf1c661c6b6305d0e1c143
drolon <drolon@users.noreply.github.com>
2011-04-10 17:14:09 +0000

    FreeImage now uses LibTIFF 3.9.5

--- FreeImage/Source/LibTIFF/tiffiop.h
+++ FreeImage/Source/LibTIFF/tiffiop.h
@@ -59,10 +59,13 @@ extern void *lfind(const void *, const void *, size_t *, size_t,
 
 /*
   Libtiff itself does not require a 64-bit type, but bundled TIFF
-  utilities may use it.
+  utilities may use it.  
 */
+
+#if !defined(__xlC__) && !defined(__xlc__) // Already defined there (#2301)
 #define TIFF_INT64_T signed long long
 #define TIFF_UINT64_T unsigned long long
+#endif
 
 #include "tiffio.h"
 #include "tif_dir.h"
