8d2f8b4435dec693961b58be9a60eab383b35b97
drolon <drolon@users.noreply.github.com>
2012-10-07 16:14:37 +0000

    fixed DDS plugin color channel swapping for RGB color order, when using FREEIMAGE_COLORORDER_RGB color order

--- FreeImage/Source/FreeImage/PluginDDS.cpp
+++ FreeImage/Source/FreeImage/PluginDDS.cpp
@@ -378,6 +378,10 @@ template <class DECODER> void DecodeDXTBlock (BYTE *dstData, const BYTE *srcBloc
 		decoder.SetY (y);
 		for (int x = 0; x < bw; x++) {
 			decoder.GetColor (x, y, (Color8888 &)*dst);
+
+#if FREEIMAGE_COLORORDER == FREEIMAGE_COLORORDER_RGB 
+			INPLACESWAP(dst[FI_RGBA_RED], dst[FI_RGBA_BLUE]);
+#endif 
 			dst += 4;
 		}
 	}
