c69486fbdc39d8f558e522c991db15fc7d63030f
Nathanael Jones <nathanael.jones@gmail.com>
2015-10-13 13:28:55 -0400

    Add win64 msvc14 command line helper

---
+++ x64_vs14_commandline.bat
@@ -0,0 +1,6 @@
+SET vcvar_arg=x86_amd64
+set bin_subdir=x64
+SET PATH=%PATH%;C:\Program Files (x86)\cmake\bin
+SET PATH=%PATH%;%LOCALAPPDATA%\nasm
+SET tbs_tools=msvc14
+cmd /k "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x86_amd64
