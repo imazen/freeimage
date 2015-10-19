b68ca74c11cf0afde91da273ee1476eda6ff4218
Nathanael Jones <nathanael.jones@gmail.com>
2015-10-19 13:02:50 -0400

    Add arch spec to x64 build helper

--- x64_vs14_commandline.bat
+++ x64_vs14_commandline.bat
@@ -4,6 +4,7 @@ SET PATH=%PATH%;C:\Program Files (x86)\cmake\bin
 SET PATH=%PATH%;%LOCALAPPDATA%\nasm
 SET vc_setup=""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x86_amd64"
 SET tbs_tools=msvc14
+SET tbs_arch=x64
 
 rem find bash from git
 rem assumes git is in [gitdir]\cmd
