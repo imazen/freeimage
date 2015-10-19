8e1143231fe9811d14547300d82d3bc2a7244174
Nathanael Jones <nathanael.jones@gmail.com>
2015-10-19 10:41:02 -0400

    Add helper scripts for windows

---
+++ dotnet_wrapper_and_tests.bat
@@ -0,0 +1,30 @@
+pushd Wrapper\FreeImage.NET\cs\
+
+bash -c "curl -L -o utd.zip https://s3.amazonaws.com/public-unit-test-resources/FreeImage.NET/UnitTestData.zip"
+bash -c "unzip utd.zip"
+popd
+
+pushd Wrapper\FreeImage.NET\cs\Library
+msbuild Library.csproj /p:Configuration=Release /v:m
+popd
+
+
+echo "Now attempting to build FreeImageIO, a nonessential C++ managed library that is out of date.
+copy Dist\FreeImage.lib Wrapper\FreeImage.NET\cpp\FreeImageIO 
+copy Dist\FreeImage.h Wrapper\FreeImage.NET\cpp\FreeImageIO 
+pushd Wrapper\FreeImage.NET\cpp\FreeImageIO 
+msbuild FreeImageIO.sln /p:Configuration=Release /v:m
+popd
+      
+      
+
+pushd Wrapper\FreeImage.NET\cs\UnitTest
+..\nuget restore -PackagesDirectory ..\packages
+msbuild UnitTest.csproj /p:Configuration=Release /v:m
+popd
+
+
+set nunit_console=Wrapper\FreeImage.NET\cs\packages\NUnit.Runners.2.6.4\tools\nunit-console
+powershell copy $(.\thumbs list_bin) "Wrapper\FreeImage.NET\cs\UnitTest\bin\Release\FreeImage.dll"
+REM %nunit_console%-x86  /framework:net-4.5 Wrapper\FreeImage.NET\cs\UnitTest\bin\Release\UnitTest.exe
+%nunit_console%  /framework:net-4.5 Wrapper\FreeImage.NET\cs\UnitTest\bin\Release\UnitTest.exe
--- x64_vs14_commandline.bat
+++ x64_vs14_commandline.bat
@@ -2,5 +2,17 @@ SET vcvar_arg=x86_amd64
 set bin_subdir=x64
 SET PATH=%PATH%;C:\Program Files (x86)\cmake\bin
 SET PATH=%PATH%;%LOCALAPPDATA%\nasm
+SET vc_setup=""C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x86_amd64"
 SET tbs_tools=msvc14
-cmd /k "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall" x86_amd64
+
+rem find bash from git
+rem assumes git is in [gitdir]\cmd
+rem and msys in [gitdir]\bin
+
+for %%i in (git.exe) do set gitexe=%%~$PATH:i
+pushd "%gitexe%\..\..\bin"
+set bashdir=%cd%
+popd
+set path=%bashdir%;%path%
+
+cmd /k %VC_SETUP%
